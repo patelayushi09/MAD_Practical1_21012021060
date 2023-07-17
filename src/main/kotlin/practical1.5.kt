d92bd21f2e756373b4103256bb98be39dcc5410a
patelayushi09 <ashapatel99246@gmail.com>
2023-07-17 21:23:01 +0530

    hii

---
+++ src/main/kotlin/practical1.5.kt
@@ -0,0 +1,16 @@
+fun main(obj:Any):String=
+        when (obj){
+            1-> "January"
+            2->"February"
+            3->"March"
+            4->"April"
+            5->"May"
+            6->"June"
+            7->"July"
+            8->"August"
+            9->"September"
+            10->"October"
+            11->"November"
+            else->"December"
+        }
+
