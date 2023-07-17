692e9d9e0628a807c083afb19c219176e4160eb9
patelayushi09 <98074206+patelayushi09@users.noreply.github.com>
2023-07-17 10:44:18 +0530

    Update practical1.1.kt

--- src/main/kotlin/practical1.1.kt
+++ src/main/kotlin/practical1.1.kt
@@ -1,3 +1,21 @@
 fun main(){
-    println("Hello World!")
+    var a:Int=9
+    var b:Float=1.5f
+    var c:String="Ayushi"
+    var d:Char='a'
+    var e:Boolean=true
+    var f:Double=9.20
+    var g:Long=95024446
+    var h:Short=2
+    var i:Byte=110
+    println("$a is an integer")
+    println("$b is a float")
+    println("$c is a string")
+    println("$d is a character")
+    println("$e is a boolean")
+    println("$f is a double value")
+    println("$g is a long value")
+    println("$h is a short value")
+    println("$i is a byte value")
+
 }
